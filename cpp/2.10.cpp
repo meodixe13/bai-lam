b4e156b0a6ddbd0a9a98ebfc4f97a5b6ae48b21c
meodixe13 <iuanhne123@gmail.com>
2016-09-21 20:21:44 +0700

    Update 2.10.cpp

--- cpp/2.10.cpp
+++ cpp/2.10.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 struct TapGoiKeo {
   int goikeo[50]; // N < 50
-  unsigned soluong, tongsokeo;
+  unsigned soluong;
 };
 
 /* CÁC HÀM CẦN XÂY DỰNG */
@@ -21,19 +21,25 @@ void xuat (TapGoiKeo nguoinhan1, TapGoiKeo nguoinhan2);
 // Hàm chia kẹo theo Giải thuật Tham lam
 void chiakeo (TapGoiKeo & nguoinhan1, TapGoiKeo & nguoinhan2, TapGoiKeo nguon);
 
-// Hàm khởi tạo
-void khoitao (TapGoiKeo & keo) {
-  keo.soluong = keo.tongsokeo = 0;
-}
-
 /* NỘI DUNG */
 
 int main () {
   TapGoiKeo nguon, nguoinhan1, nguoinhan2;
   nhap(nguon);
-  khoitao(nguoinhan1);
-  khoitao(nguoinhan2);
   chiakeo(nguoinhan1, nguoinhan2, nguon);
   xuat(nguoinhan1, nguoinhan2);
   return 0;
+}
+
+int nhap(TapGoiKeo &nguon)
+{
+  int n;
+  cout<<"Nhap so goi keo (<50): ";
+  cin>>n;
+  for(int i = 0; i < n; i++)
+  {
+    cout<<"Nhap so luong goi keo thu "<<nguon.goikeo[i]<<":";
+    cin>>nguon.soluong;
+    nguon.soluong += nguon.soluong;
+  }
 }
