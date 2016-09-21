c18a6db228b66546016d74f96b53343b19ab2cbf
meodixe13 <iuanhne123@gmail.com>
2016-09-21 20:24:06 +0700

    Update 2.10.cpp

--- cpp/2.10.cpp
+++ cpp/2.10.cpp
@@ -37,3 +37,48 @@ int main () {
   xuat(nguoinhan1, nguoinhan2);
   return 0;
 }
+#include <iostream>
+
+using namespace std;
+
+struct TapGoiKeo {
+  int goikeo[50]; // N < 50
+  unsigned soluong;
+};
+
+/* CÁC HÀM CẦN XÂY DỰNG */
+
+// Chương trình chính
+int main ();
+
+// Hàm nhập: Nhập số lượng và các gói kẹo được chia
+void nhap (TapGoiKeo & nguon);
+
+// Hàm xuất
+void xuat (TapGoiKeo nguoinhan1, TapGoiKeo nguoinhan2);
+
+// Hàm chia kẹo theo Giải thuật Tham lam
+void chiakeo (TapGoiKeo & nguoinhan1, TapGoiKeo & nguoinhan2, TapGoiKeo nguon);
+
+/* NỘI DUNG */
+
+int main () {
+  TapGoiKeo nguon, nguoinhan1, nguoinhan2;
+  nhap(nguon);
+  chiakeo(nguoinhan1, nguoinhan2, nguon);
+  xuat(nguoinhan1, nguoinhan2);
+  return 0;
+}
+
+int nhap(TapGoiKeo &nguon)
+{
+  int n;
+  cout<<"Nhap so goi keo (<50): ";
+  cin>>n;
+  for(int i = 0; i < n; i++)
+  {
+    cout<<"Nhap so luong goi keo thu "<<nguon.goikeo[i]<<":";
+    cin>>nguon.soluong;
+    nguon.soluong += nguon.soluong;
+  }
+}
