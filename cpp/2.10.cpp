b86b09cfe63e2d11094d49ea0c4e2fd6a7e57b22
khai96_ <hvksmr1996@gmail.com>
2016-09-21 19:52:39 +0700

    Bắt đầu câu 2.10

---
+++ cpp/2.10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct TapGoiKeo {
+  int goikeo[50]; // N < 50
+  unsigned soluong;
+};
+
+/* CÁC HÀM CẦN XÂY DỰNG */
+
+// Chương trình chính
+int main ();
+
+// Hàm nhập: Nhập số lượng và các gói kẹo được chia
+void nhap (TapGoiKeo &nguon);
+
+// Hàm xuất
+void xuat (TapGoiKeo nguoinhan1, TapGoiKeo nguoinhan2);
+
+// Hàm chia kẹo theo Giải thuật Tham lam
+void chiakeo (TapGoiKeo & nguoinhan1, TapGoiKeo & nguoinhan2, TapGoiKeo nguon);
